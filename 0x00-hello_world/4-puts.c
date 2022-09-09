df04c726400671c738e8d5ae051eeb1189b313e4
oluwatimtex <oluwatimtex@outlook.com>
2022-09-08 17:14:52 -0700

    my first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ *this is a documentation of the below main function
+ */
+
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
