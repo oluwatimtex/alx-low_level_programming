1a90a84a46d047ce1a1d54f271310f710595ba95
oluwatimtex <oluwatimtex@outlook.com>
2022-09-08 18:59:54 -0700

    print using standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main- the entry to every c program
+ *
+ * Return: function returns 1 which is an int
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
