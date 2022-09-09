03d5d10962f56886aa5ac226030c128417e72786
oluwatimtex <oluwatimtex@outlook.com>
2022-09-08 17:24:55 -0700

    my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 
 /**
+ *
  *this is a documentation of the below main function
+ *main this is the entry point of the program
+ *return value is 0 cause its a int function
  */
 
 int main(void)
 {
 
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 
 }
