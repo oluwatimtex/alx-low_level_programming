dc169962262491c93c1fc440c01fb0f3514836da
oluwatimtex <oluwatimtex@outlook.com>
2022-09-08 17:47:24 -0700

    my second c program using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- the main is the entry point to all c program
+ *
+ * Return: it returns 0 cause its and int function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
