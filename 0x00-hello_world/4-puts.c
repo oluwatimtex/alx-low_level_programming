2ddcf5c41e19e8d56ccdd1a7e0f5b1d9097c8feb
oluwatimtex <oluwatimtex@outlook.com>
2022-09-08 17:32:17 -0700

    my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 /**
+ * this is a documentation of the below main function
  *
- *this is a documentation of the below main function
- *main this is the entry point of the program
- *return value is 0 cause its a int function
+ * main - this is the entry point of the program
+ * Return - value is 0 cause its a int function
  */
 
 int main(void)
