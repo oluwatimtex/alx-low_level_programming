040ef43a4e2c084058285ec8f2bbcbdf65cf0a52
oluwatimtex <oluwatimtex@outlook.com>
2022-09-10 10:25:03 -0700

    printing single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - the main function prints the single digit numbers of base 10 0-9
+ *
+ * Return: main function returns 0
+ */
+
+int main(void)
+{
+	int single_digit;
+
+	for (single_digit = 0; single_digit < 10; single_digit++)
+	{
+		printf("%d\n", single_digit);
+	}
+	return (0);
+
+}
