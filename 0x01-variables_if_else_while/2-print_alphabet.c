95a9b8f0913b5ce7ebe7c7fb5067d2170d430ce1
oluwatimtex <oluwatimtex@outlook.com>
2022-09-10 08:18:34 -0700

    printing lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the alphabet in low case
+ *
+ * Return: main function returns 0
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(alphabet);
+
+	return (0);
+}
