e9f4e205a3c893300e3da525b168bc5e585a6be4
oluwatimtex <oluwatimtex@outlook.com>
2022-09-10 11:04:32 -0700

    printing single digit numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int digit;
 
-	for (digit = 28; digit < 38; digit++)
+	for (digit = 0; digit < 10; digit++)
 	{
-		putchar(digit);
+		putchar((int)digit);
 	}
 	putchar('\n');
 	return (0);
