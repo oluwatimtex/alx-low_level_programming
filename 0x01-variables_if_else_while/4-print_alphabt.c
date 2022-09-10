0592d782eb7ff4459dc98bc71906ccc195bf2d3c
oluwatimtex <oluwatimtex@outlook.com>
2022-09-10 09:44:08 -0700

    print all alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Prints the alphabet except q and e.
+ *
+ * Return: function returns 0
+ */
+
+int main(void)
+
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int character;
+
+	for (character = 0; character < 26; character++)
+	{
+		if (alphabet[character] != q && alphabet[character] != e)
+		{
+			putchar(alphabet[character]);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+
+}
