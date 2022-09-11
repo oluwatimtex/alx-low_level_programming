3aaa506ac9d7b5d8cd98790726d41b303db6cc37
oluwatimtex <oluwatimtex@outlook.com>
2022-09-11 00:43:40 -0700

    combo of all single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,14 @@
  */
 int main(void)
 {
-	int combo[2][10] = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}};
-	int counter1, counter2;
+	int combo[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int counter1;
 
-	for (counter1 = 0; counter1 < 2; counter1++)
+	for (counter1 = 0; counter1 < 10; counter1++)
 	{
-		for (counter2 = 0; counter2 < 10; counter2++)
-		{
-			putchar((combo[counter1][counter2] % 10) + '0');
-			putchar(',');
-			putchar(' ');
+		putchar((combo[counter1] % 10) + '0');
+		putchar(',');
+		putchar(' ');
 		}
 	}
 	putchar('\n');
