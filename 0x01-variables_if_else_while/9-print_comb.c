15551d6cea6954032232088180825ce060cc85b2
oluwatimtex <oluwatimtex@outlook.com>
2022-09-11 00:34:29 -0700

    combo of all signle digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - the main function returns all combo of single digit
+ *
+ * Return: the main function returns 0
+ */
+int main(void)
+{
+	int combo[2][10] = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}};
+	int counter1, counter2;
+
+	for (counter1 = 0; counter1 < 2; counter1++)
+	{
+		for (counter2 = 0; counter2 < 10; counter2++)
+		{
+			putchar((combo[counter1][counter2] % 10) + '0');
+			putchar(', ');
+		}
+	putchar('\n');
+	return (0);
+}
