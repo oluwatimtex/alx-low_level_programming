57b7ae637fbaea822c021c09390a5fcb09ebf7c1
oluwatimtex <oluwatimtex@outlook.com>
2022-09-12 15:23:40 -0700

    all combo of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,19 +10,19 @@ int main(void)
 	int i;
 	int j;
 	int k;
-	int ten = 10;
+
 
 	for (i = '0'; i <= '8'; i++)
 	{
 		for (j = i + 1 ; j <= '9'; j++)
 		{
-			for(k = j + 1; k < 'ten'; k++)
+			for(k = j + 1; k < 10; k++)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
 
-				if (i == '8' && j == '9' && k == 'ten')
+				if (i == '8' && j == '9' && k == 10)
 				{
 					putchar('\n');
 				}
