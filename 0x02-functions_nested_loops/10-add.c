ee8d91c0b87f6426ec8608eb7e65a122ce45fe9c
oluwatimtex <oluwatimtex@outlook.com>
2022-09-14 15:03:22 -0700

    adds 2 value together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * add - integer type unction to add 2 digits together
+ *
+ * @n and @c - the 2 integer type parameters 
+ *
+ * Return: function returns the total of 2 digits
+ */
+int add(int n, int c)
+{
+	int total;
+	total = n + c;
+	return (total);
+
+}
+
