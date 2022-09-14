c9a351496a0ff44838bd3196548e69d8b21ce878
oluwatimtex <oluwatimtex@outlook.com>
2022-09-14 09:40:59 -0700

    checks for lowercase letter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,8 @@
 /**
  * _islower - checks for lower and upper case letters
  *
+ * c - c is a parameter that is of type int
+ *
  * Return: function returns 1 for lower case and 0 for uppercase
  */
 
