f467c8427647a0ddf4161bf48a06ba9f3693c8a1
oluwatimtex <oluwatimtex@outlook.com>
2022-09-14 12:29:01 -0700

    display signs of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,9 +4,9 @@
 /**
  * print_sign - prints the sign of a number
  *
- * n: n is a parameter and is of type integer
+ * @n: n is a parameter and is of type integer
  *
- * Return: function returns depending on the parameter n- 1,0 or -1  
+ * Return: function returns depending on the parameter n- 1,0 or -1
  */
 int print_sign(int n)
 {
