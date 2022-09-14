26d54e32be6e1f188a7164aad451b8dd4cb2eb83
oluwatimtex <oluwatimtex@outlook.com>
2022-09-13 19:39:34 -0700

    display putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - the main function prints _putchar
+ *
+ * Return: the main function returns 0
+ */
+int main(void)
+{
+	char print[8]= '_putchar';
+
+	putchar ('print');
+	return (0);
+
+}
