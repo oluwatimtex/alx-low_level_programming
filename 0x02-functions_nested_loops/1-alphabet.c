283cae24102e6cc5422c6f4fe8be19abf8293ef9
oluwatimtex <oluwatimtex@outlook.com>
2022-09-14 09:07:55 -0700

    print alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * main - function print the content of function print_alphabet
  * print_alphabet - prints alphaet in small letters
+ *
  * Return: main function return 0
  */
 void print_alphabet(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
