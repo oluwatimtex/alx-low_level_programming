c518b39eb07da23781afcb8d58c2133b7f93130c
oluwatimtex <oluwatimtex@outlook.com>
2022-09-14 11:07:14 -0700

    displays sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * n: n is a parameter and is of type integer
+ *
+ * Return: function returns depending on the parameter n- 1,0 or -1  
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+		return (-1);
+
+}
