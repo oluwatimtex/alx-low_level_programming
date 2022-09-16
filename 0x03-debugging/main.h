d753665fe2a08a1adc51bb0bd41e981675a2a11a
oluwatimtex <oluwatimtex@outlook.com>
2022-09-15 18:19:10 -0700

    first debug for postive or negative number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-#include<stdio.h>
+#include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
@@ -9,6 +9,4 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
 
-#endif // MAIN_H_INCLUDED
-
-
+#endif /* MAIN_H */
