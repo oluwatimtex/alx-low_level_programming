a42e8b7b0370b98e516ef1a8b69a337b1c6e4ae2
oluwatimtex <oluwatimtex@outlook.com>
2022-09-16 17:08:46 -0700

    prints number betwen 0-14 * 10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0-14, 10 times.
+ * Return: no return.
+ */
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num >= 10)
+				_putchar((num / 10) + 48);
+			_putchar((num % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
