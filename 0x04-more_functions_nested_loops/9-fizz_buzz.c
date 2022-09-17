7a0cb59bbbfefb443a4862bd0ba456fea3b76742
oluwatimtex <oluwatimtex@outlook.com>
2022-09-16 18:04:28 -0700

    FizzBuzz prints Buzz for 5 and Fizz for 3

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	num = 1;
+	printf("%d", num);
+	for (num = 2; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
