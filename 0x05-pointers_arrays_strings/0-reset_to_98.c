33efda0ce752660fde8c4446c008ced16fefd339
oluwatimtex <oluwatimtex@outlook.com>
2022-09-19 16:32:36 -0700

    update reference of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - update the reference of a pointer
+ *
+ * @n: pointer to integer number
+ *
+ * Return: void function returns nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is the intro into pointers
