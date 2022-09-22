c65b4467afc10967c31a5110a032e8474346e983
root <root@Angry-bird.localdomain>
2022-09-22 02:28:26 +0100

    concatenate 2 strings pointer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,9 +6,11 @@
  * @src: source.
  * Return: the pointer to dest.
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+	int count = 0;
+	int count2 = 0;
 
 	while (*(dest + count) != '\0')
 	{
