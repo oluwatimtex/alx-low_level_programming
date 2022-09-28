3086ffa788285ba6efba5490c258eb1835842531
root <root@Angry-bird.localdomain>
2022-09-29 00:38:33 +0100

    returns the  power of a number with a given number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function returns the value of x raised to y
+ * @x: is an int parameter which is a whole number
+ * @y: is an int parameter which is the power of x
+ * Return: returns an int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (0);
+	else
+		return (x ** y);
+}
