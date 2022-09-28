49e628e42d3bd2790f8a9aa9e675cccbea728224
root <root@Angry-bird.localdomain>
2022-09-28 15:14:43 +0100

    prints a string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: is a parameter that recieves the string
+ * Return: void function returns nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
+	else
+		_putchar('\n');
+}
