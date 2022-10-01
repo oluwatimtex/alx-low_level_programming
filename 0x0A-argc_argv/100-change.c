b50bf10bb0103d04012d40865909b4bb7cb1401d
root <root@Angry-bird.localdomain>
2022-10-01 04:49:34 +0100

    change for an amount

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change 
+ * main - prints the minimum number of coins to make change
  * for an amount of money.
  * @argc: number of command line arguments.
  * @argv: array that contains argc
