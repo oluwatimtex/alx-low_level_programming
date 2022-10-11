aa4156c286ef937930697d6df1976fb2a842534d
root <root@Angry-bird.localdomain>
2022-10-11 20:25:12 +0100

    define a structure of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - describing characters of a dog
+ * @name: describes the name of the dog
+ * @age: describes the age of the dog
+ * @owner: describes the owner of thedog
+ *
+ * Description: describing a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
