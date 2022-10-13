112ee6b193bd1b32901bb05098d703cbc38c8062
root <root@Angry-bird.localdomain>
2022-10-13 15:09:43 +0100

    sum of parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
+ *
+ * Return: sum of its parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int count;
+	int sum = 0;
+	va_list list;
+	
+	if (n == 0)
+		return (0);
+
+	va_start (list, n);
+
+	for (count = 0; count < n; count++)
+		sum = sum + va_arg(list, int);
+	
+	va_end (list);
+
+	return (sum);
+}
