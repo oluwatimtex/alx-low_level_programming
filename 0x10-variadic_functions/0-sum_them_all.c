d610e85d6d62a13fa01e1377d78a1a0a0b5c62d0
root <root@Angry-bird.localdomain>
2022-10-13 15:16:52 +0100

    sum of parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list lists;
 	unsigned int count;
 	int sum = 0;
-	va_list list;
 
 	if (n == 0)
 		return (0);
 
-	va_start(list, n);
+	va_start(lists, n);
 
 	for (count = 0; count < n; count++)
-		sum = sum + va_arg(list, int);
+		sum = sum + va_arg(lists, int);
 
-	va_end(list);
+	va_end(lists);
 
 	return (sum);
 }
